73ac49d49d6b0e540c965028b3a4098bdc6ac5da
Heesu974 <heesu.kang95@gmail.com>
2021-07-14 21:27:12 +0900

    update through all

--- js/custom.js
+++ js/custom.js
@@ -7,42 +7,6 @@ $(window).scroll(function () {
 })
 
 
-// slick js
-$('.myslider').slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-                infinite: true,
-                dots: true
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-    ]
-});
 
 // trigger
 $('.trigger').click(function () {
@@ -56,50 +20,29 @@ $('.gnb a').click(function () {
 
 
 
-//main-header 
+//main-header --삭제함
 
-const item = document.querySelectorAll('.item');
-console.log(item);
-const itemArr = Array.from(item);
-console.log(itemArr);
+// const item = document.querySelectorAll('.item');
+// console.log(item);
+// const itemArr = Array.from(item);
+// console.log(itemArr);
 
-function slideUpHandler(e) {
-    console.log(e.target.next);
-    e.target.classList.add('active');
-}
-function slideDownHandler(e) {
-    console.log(e.target.next);
-    e.target.classList.remove('active');
-}
-for (let i = 0; i < itemArr.length; i++) {
-    itemArr[i].addEventListener('mouseenter', slideUpHandler)
-}
-for (let i = 0; i < itemArr.length; i++) {
-    itemArr[i].addEventListener('mouseleave', slideDownHandler)
-}
+// function slideUpHandler(e) {
+//     console.log(e.target.next);
+//     e.target.classList.add('active');
+// }
+// function slideDownHandler(e) {
+//     console.log(e.target.next);
+//     e.target.classList.remove('active');
+// }
+// for (let i = 0; i < itemArr.length; i++) {
+//     itemArr[i].addEventListener('mouseenter', slideUpHandler)
+// }
+// for (let i = 0; i < itemArr.length; i++) {
+//     itemArr[i].addEventListener('mouseleave', slideDownHandler)
+// }
 
-//section-second 
 
-const newsCard = document.getElementsByClassName('news-card');
-console.log(newsCard);
-const newsCardArr = Array.from(newsCard);
-console.log(newsCardArr);
-
-function changeColorHandler(e) {
-
-    e.target.classList.add('fill');
-}
-function changeColorHandler2(e) {
-
-    e.target.classList.remove('fill');
-}
-
-for (let i = 0; i < newsCardArr.length; i++) {
-    newsCardArr[i].addEventListener('mouseenter', changeColorHandler);
-}
-for (let i = 0; i < newsCardArr.length; i++) {
-    newsCardArr[i].addEventListener('mouseleave', changeColorHandler2);
-}
 
 
 //main-intro 
@@ -120,6 +63,8 @@ function typing(e) {
 
 }
 typing();
+
+
 
 
 // // main-section-second-thumnail
@@ -163,8 +108,107 @@ for (let i = 0; i < subArr.length; i++) {
 
 }
 
+// main-section-second-slick js
+$('.myslider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    prevArrow: '.arrow_prev',
+    nextArrow: '.arrow_next',
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
 
 
 
+//section-fourth 
+
+const newsCard = document.getElementsByClassName('news-card');
+console.log(newsCard);
+const newsCardArr = Array.from(newsCard);
+console.log(newsCardArr);
+
+function changeColorHandler(e) {
+
+    e.target.classList.add('fill');
+}
+function changeColorHandler2(e) {
+
+    e.target.classList.remove('fill');
+}
+
+for (let i = 0; i < newsCardArr.length; i++) {
+    newsCardArr[i].addEventListener('mouseenter', changeColorHandler);
+}
+for (let i = 0; i < newsCardArr.length; i++) {
+    newsCardArr[i].addEventListener('mouseleave', changeColorHandler2);
+}
+
+
+
+//main-section-first 
+
+const modalBtns = document.querySelectorAll('.modalBtn');
+console.log(modalBtns);
+const modalBtnArr = Array.from(modalBtns);
+console.dir(modalBtnArr);
+const modal = document.querySelectorAll('.modal');
+console.log(modal);
+const modalArr = Array.from(modal);
+console.dir(modalBtnArr);
+const modalAttr = modalBtnArr.filter(item => item.getAttribute('data-alt'));
+console.log(modalAttr);
+function modalOpenHandler(e) {
+    e.preventDefault();
+    let targetAttr = e.target.getAttribute('data-alt');
+
+    const modalTarget = modalArr.find(item => item.classList.contains(targetAttr));
+
+
+    modalTarget.classList.add('open');
+
+    return (modalTarget);
+}
+function modalCloseHandler(e) {
+    console.log(e.target);
+    e.target.classList.remove('open');
+}
+
+for (let i = 0; i < modalBtnArr.length; i++) {
+
+    modalBtnArr[i].addEventListener('click', modalOpenHandler);
+}
+
+for (let i = 0; i < modalArr.length; i++) {
+    modalArr[i].addEventListener('click', modalCloseHandler); ''
+}
 
 
